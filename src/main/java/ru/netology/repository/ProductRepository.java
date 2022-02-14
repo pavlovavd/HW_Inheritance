5604704d4725083f6d55a7e2aad8f32469534cf6
Victoria Pavlova <pvd0510@mail.ru>
2022-02-14 16:29:37 +0300

    Initial commit

---
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -0,0 +1,42 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Product;
+
+public class ProductRepository {
+    private Product[] products = new Product[0];
+
+    public void save(Product product) {
+        int length = products.length + 1;
+        Product[] tmp = new Product[length];
+        System.arraycopy(products, 0, tmp, 0, products.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = product;
+        products = tmp;
+    }
+
+    public Product[] findAll() {
+        return products;
+    }
+
+    public Product findById(int id) {
+        for (Product item : products) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        int length = products.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : products) {
+            if (item.getId() != id) {
+            tmp[index] = item;
+            index++;
+            }
+        }
+        products = tmp;
+    }
+}
