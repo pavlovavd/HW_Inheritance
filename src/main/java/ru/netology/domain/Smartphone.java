8c14dfdd5c2227da0b35fe5623ee2600a53f4244
Victoria Pavlova <pvd0510@mail.ru>
2022-02-14 20:09:40 +0300

    Reform ProductRepositoryTest.java and Smartphone.java

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -2,10 +2,15 @@ package ru.netology.domain;
 
 import java.util.Objects;
 
-public class Smartphone  extends Product{
+public class Smartphone extends Product {
     private String manufacturer;
 
-    public Smartphone(String manufacturer) {
+    public Smartphone() {
+        super();
+    }
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
         this.manufacturer = manufacturer;
     }
 
--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -3,66 +3,81 @@ package ru.netology.repository;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
+
+    @Test
+    public void shouldSaveOneItem () {
+        ProductRepository repo = new ProductRepository();
+        Book Inspector = new Book();
+
+        repo.save(Inspector);
+        repo.findById(1);
+
+        Product[] expected = new Product[]{Inspector};
+        Product[] actual = repo.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSaveTwoItem () {
+        ProductRepository repo = new ProductRepository();
+        Book Inspector = new Book();
+        Smartphone IPhone = new Smartphone();
+
+        repo.save(Inspector);
+        repo.save(IPhone);
+
+        Product[] expected = new Product[]{Inspector, IPhone};
+        Product[] actual = repo.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
     @Test
     public void shouldRemoveById() {
         ProductRepository repo = new ProductRepository();
-
         int idToRemove = 1;
-
         Product first = new Product(1, "first", 100);
-        Product second = new Product(2, "second", 200);
-        Product third = new Product(3, "third", 300);
+        Book Inspector = new Book (2, "Ревизор", 200, "М.Горький");
+        Smartphone IPhone = new Smartphone(3, "IPhone", 500, "Apple");
 
         repo.save(first);
-        repo.save(second);
-        repo.save(third);
-
+        repo.save(Inspector);
+        repo.save(IPhone);
         repo.removeById(idToRemove);
 
-        Product[] expected = new Product[]{second, third};
+        Product[] expected = new Product[]{Inspector, IPhone};
         Product[] actual = repo.findAll();
-
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void shouldFindByIdOneProduct() {
         ProductRepository repo = new ProductRepository();
-        int idToFind = 1;
+        Book Inspector = new Book (1, "Ревизор", 200, "М.Горький");
 
-        Product first = new Product(1, "first", 100);
+        repo.save(Inspector);
+        repo.findById(1);
 
-        repo.save(first);
-
-        repo.findById(idToFind);
-
-        Product[] expected = new Product[]{first};
+        Product[] expected = new Product[] {Inspector};
         Product[] actual = repo.findAll();
-
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void shouldFindByIdTwoProduct() {
         ProductRepository repo = new ProductRepository();
+        Book Inspector = new Book (2, "Ревизор", 200, "М.Горький");
+        Smartphone IPhone = new Smartphone(3, "IPhone", 500, "Apple");
 
-        Product first = new Product(1, "first", 100);
-        Product second = new Product(1, "second", 100);
+        repo.save(Inspector);
+        repo.save(IPhone);
 
-        repo.save(first);
-        repo.save(second);
-
-        repo.findById(1);
-        repo.findById(2);
-
-        Product[] expected = new Product[]{first, second};
+        Product[] expected = new Product[]{Inspector, IPhone};
         Product[] actual = repo.findAll();
-
         assertArrayEquals(expected, actual);
     }
-
 }
