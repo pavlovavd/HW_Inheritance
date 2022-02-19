17eecbf436b3cbb0d0bdcda0a0b0424c128a7c3e
Victoria Pavlova <pvd0510@mail.ru>
2022-02-19 15:56:02 +0300

    Modified ProductRepository.java and ProductRepositoryTest.java

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -30,7 +30,7 @@ public class ProductRepository {
 
     public void removeById(int id) {
         if (findById(id) == null) {
-            throw new NotFoundException("Element with id: " + id + " not found")
+            throw new NotFoundException("Element with id: " + id + " not found");
         }
         int length = products.length - 1;
         Product[] tmp = new Product[length];
--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,39 +1,29 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
 import ru.netology.domain.Smartphone;
+import ru.netology.exceptions.NotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
 
     @Test
-    public void shouldSaveOneItem () {
+    public void shouldNotFoundException() {
         ProductRepository repo = new ProductRepository();
-        Book Inspector = new Book();
+        Product first = new Product(1, "first", 100);
+        Book Inspector = new Book(2, "Ревизор", 200, "М.Горький");
+        Smartphone IPhone = new Smartphone(3, "IPhone", 500, "Apple");
 
-        repo.save(Inspector);
-        repo.findById(1);
-
-        Product[] expected = new Product[]{Inspector};
-        Product[] actual = repo.findAll();
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSaveTwoItem () {
-        ProductRepository repo = new ProductRepository();
-        Book Inspector = new Book();
-        Smartphone IPhone = new Smartphone();
-
+        repo.save(first);
         repo.save(Inspector);
         repo.save(IPhone);
 
-        Product[] expected = new Product[]{Inspector, IPhone};
-        Product[] actual = repo.findAll();
-        assertArrayEquals(expected, actual);
+        Assertions.assertThrows(NotFoundException.class, () -> repo.removeById(4));
+
     }
 
     @Test
@@ -41,7 +31,7 @@ class ProductRepositoryTest {
         ProductRepository repo = new ProductRepository();
         int idToRemove = 1;
         Product first = new Product(1, "first", 100);
-        Book Inspector = new Book (2, "Ревизор", 200, "М.Горький");
+        Book Inspector = new Book(2, "Ревизор", 200, "М.Горький");
         Smartphone IPhone = new Smartphone(3, "IPhone", 500, "Apple");
 
         repo.save(first);
@@ -55,14 +45,41 @@ class ProductRepositoryTest {
     }
 
     @Test
-    public void shouldFindByIdOneProduct() {
+    public void shouldSaveOneItem() {
         ProductRepository repo = new ProductRepository();
-        Book Inspector = new Book (1, "Ревизор", 200, "М.Горький");
+        Book Inspector = new Book();
 
         repo.save(Inspector);
         repo.findById(1);
 
-        Product[] expected = new Product[] {Inspector};
+        Product[] expected = new Product[]{Inspector};
+        Product[] actual = repo.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSaveTwoItem() {
+        ProductRepository repo = new ProductRepository();
+        Book Inspector = new Book();
+        Smartphone IPhone = new Smartphone();
+
+        repo.save(Inspector);
+        repo.save(IPhone);
+
+        Product[] expected = new Product[]{Inspector, IPhone};
+        Product[] actual = repo.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindByIdOneProduct() {
+        ProductRepository repo = new ProductRepository();
+        Book Inspector = new Book(1, "Ревизор", 200, "М.Горький");
+
+        repo.save(Inspector);
+        repo.findById(1);
+
+        Product[] expected = new Product[]{Inspector};
         Product[] actual = repo.findAll();
         assertArrayEquals(expected, actual);
     }
@@ -70,7 +87,7 @@ class ProductRepositoryTest {
     @Test
     public void shouldFindByIdTwoProduct() {
         ProductRepository repo = new ProductRepository();
-        Book Inspector = new Book (2, "Ревизор", 200, "М.Горький");
+        Book Inspector = new Book(2, "Ревизор", 200, "М.Горький");
         Smartphone IPhone = new Smartphone(3, "IPhone", 500, "Apple");
 
         repo.save(Inspector);
